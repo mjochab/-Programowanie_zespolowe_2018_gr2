90b75558745fbff8c10ba9ecfbee6983146361e3
Sebastian Zabrzyski <sebastianzabrzyski@gmail.com>
2018-04-17 22:13:03 +0200

    javadoc

--- src/warsztatsamochodowy/controllers/UserSettingsController.java
+++ src/warsztatsamochodowy/controllers/UserSettingsController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package warsztatsamochodowy.controllers;
 
 import java.io.IOException;
@@ -17,7 +12,7 @@ import warsztatsamochodowy.Helper;
 import warsztatsamochodowy.database.DatabaseConnection;
 
 /**
- * FXML Controller class
+ * Klasa kontrolera FXML do zmiany ustawień zalogowanego użytkownika.
  *
  * @author lukasz, sebastian
  */
